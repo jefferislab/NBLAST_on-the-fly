e9b205e8df09f430bc02337d0426b7101d0e6972
James Manton <ajd.manton@googlemail.com>
2014-09-26 14:31:17 +0100

    Add neuron selection to side panel and implement blasting

--- server.R
+++ server.R
@@ -4,6 +4,14 @@ library(nat.nblast)
 library(flycircuit)
 library(shiny)
 library(shinyRGL)
+library(shinysky)
+
+# Load dps object for plotting neurons
+dps <- read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/dpscanon_f9dc90ce5b2ffb74af37db1e3a2cb35b.rds", localdir=getOption('flycircuit.datadir'))
+
+# Attach the all-by-all score matrix and load into memory
+allbyall <- fc_attach_bigmat("allbyallblastcanon_f9dc90ce5b2ffb74af37db1e3a2cb35b")
+allbyallmem <- allbyall[, ]
 
 # Define a function for a frontal view of the brain
 frontalView<-function(zoom=0.6){
@@ -14,8 +22,29 @@ frontalView<-function(zoom=0.6){
 shinyServer(function(input, output) {
 
 output$brain3d <- renderWebGL({
-  plot3d(FCWB)
-  frontalView()
+  query_neuron <- input$query
+  target_neuron <- input$target
+  if(is.null(query_neuron) || is.null(target_neuron)) {
+    # Dummy plot
+    plot3d(FCWB)
+    frontalView()
+  } else {
+    clear3d()
+    plot3d(dps[fc_gene_name(query_neuron)], col='red')
+    plot3d(dps[fc_gene_name(target_neuron)], col='blue')
+    plot3d(FCWB)
+    frontalView()
+  }
+})
+
+output$nblast_results <- renderText({
+  query_neuron <- input$query
+  target_neuron <- input$target
+  if(is.null(query_neuron) || is.null(target_neuron)) {
+    ""
+  } else {
+    fc_nblast(fc_gene_name(query_neuron), fc_gene_name(target_neuron), scoremat=allbyallmem)
+  }
 })
 
 })
--- ui.R
+++ ui.R
@@ -1,16 +1,45 @@
 library(shiny)
 library(shinyRGL)
 
+dps <- read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/dpscanon_f9dc90ce5b2ffb74af37db1e3a2cb35b.rds", localdir=getOption('flycircuit.datadir'))
+
+neuron_names <- fc_neuron(names(dps))
+neuron_ids <- fc_idid(names(dps))
+
 shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel("Compare neuron similarities with NBLAST"),
   
-  sidebarPanel(),
+  sidebarPanel(
+    h3("Query:"),
+    textInput.typeahead(
+      id="query",
+      placeholder="Type a FlyCircuit neuron name",
+      local=data.frame(name=neuron_names, id=neuron_ids),
+      valueKey = "name",
+      tokens=1:length(neuron_names),
+      template = HTML("<p class='repo-language'>{{id}}</p> <p class='repo-name'>{{name}}</p>")
+    ),
+    
+    h3("Target:"),
+    textInput.typeahead(
+      id="target",
+      placeholder="Type a FlyCircuit neuron name",
+      local=data.frame(name=neuron_names, id=neuron_ids),
+      valueKey = "name",
+      tokens=1:length(neuron_names),
+      template = HTML("<p class='repo-language'>{{id}}</p> <p class='repo-name'>{{name}}</p>")
+    ),
+    br(),
+    br(),
+    submitButton("NBLAST")
+  ),
   
   mainPanel(
     h2("3D view"),
     webGLOutput("brain3d"),
-    h2("NBLAST results")
-    )
+    h2("NBLAST results"),
+    textOutput("nblast_results")
+  )
 ))
