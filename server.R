121c3ac4c6781512a06e03d8e639dab5f2e0f5d8
James Manton <ajd.manton@googlemail.com>
2014-10-02 22:22:59 +0100

    Add normalised scores to NBLAST results display

--- server.R
+++ server.R
@@ -51,7 +51,7 @@ output$nblast_results_one <- renderText({
   if(query_neuron == "" || target_neuron == "") {
     ""
   } else {
-    fc_nblast(fc_gene_name(query_neuron), fc_gene_name(target_neuron), scoremat=allbyall)
+    paste0("Raw score: ", fc_nblast(fc_gene_name(query_neuron), fc_gene_name(target_neuron), scoremat=allbyall), "    |    Normalised score: ", fc_nblast(fc_gene_name(query_neuron), fc_gene_name(target_neuron), scoremat=allbyall) / fc_nblast(fc_gene_name(query_neuron), fc_gene_name(query_neuron), scoremat=allbyall))
   }
 })
 
@@ -80,7 +80,7 @@ output$nblast_results_all <- renderPlot({
     NULL
   } else {
     scores <- fc_nblast(fc_gene_name(query_neuron), scoremat=allbyall)
-    output$nblast_results_all_top10 <- renderTable({ data.frame(scores=sort(scores, decreasing=TRUE)[2:11]) })
+    output$nblast_results_all_top10 <- renderTable({ data.frame(scores=sort(scores, decreasing=TRUE)[2:11], normalised_scores=sort(scores/fc_nblast(fc_gene_name(query_neuron), fc_gene_name(query_neuron), scoremat=allbyall), decreasing=TRUE)[2:11]) })
     nblast_results <- data.frame(scores=scores)
     p <- ggplot(nblast_results, aes(x=scores)) + geom_histogram(binwidth=diff(range(nblast_results$scores))/100) + xlab("NBLAST score") + ylab("Frequency density") + geom_vline(xintercept=0, colour='red')
     p
@@ -116,7 +116,7 @@ output$nblast_results_tracing <- renderPlot({
     if(grepl("\\.swc", query_neuron$name)) tracing_neuron <- nat:::read.neuron.swc(query_neuron$datapath)
     else tracing_neuron <- read.neuron(query_neuron$datapath)
     message(system.time(scores <- nblast(dotprops(tracing_neuron), dps[exemplars])))
-    output$nblast_results_all_top10 <- renderTable({ data.frame(scores=sort(scores, decreasing=TRUE)[1:10]) })
+    output$nblast_results_tracing_top10 <- renderTable({ data.frame(scores=sort(scores, decreasing=TRUE)[1:10], normalised_scores=sort(scores/nblast(dotprops(tracing_neuron), dotprops(tracing_neuron)), decreasing=TRUE)[1:10]) })
     nblast_results <- data.frame(scores=scores)
     p <- ggplot(nblast_results, aes(x=scores)) + geom_histogram(binwidth=diff(range(nblast_results$scores))/100) + xlab("NBLAST score") + ylab("Frequency density") + geom_vline(xintercept=0, colour='red')
     p
