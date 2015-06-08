7c2f13f4d5c5e0e05805f841349ffb623f826a79
James Manton <ajd.manton@googlemail.com>
2015-06-08 22:15:39 +0100

    Improve positioning of loading spinner

--- ui.R
+++ ui.R
@@ -31,13 +31,6 @@ shinyUI(navbarPage("NBLAST on-the-fly",
         includeHTML("url.js"),
         hashProxy("hash"),
         
-        
-        ###################
-        # Loading spinner #
-        ###################
-        includeCSS("loader.css"),
-        HTML("<div class='loader' style='position: absolute; left: 940px; top: 300px; z-index: -10000;'>Loading...</div>"),
-        
         h3("Instructions"),
         HTML("Select a FlyCircuit neuron to compare against all FlyCircuit neurons, with NBLAST. If the checkbox below is ticked, both forwards and reverse scores will be calculated, normalised and averaged, rather than just using the forwards score. The query neuron will be <b><span style='color: black;'>plotted in black</span></b> in the 3D viewer to the right, alongside the top 10 hits (rainbow coloured from <span style='color: red;'>red = best</span> to <span style='color: #FF0099;'>pink = worst</span>)."),
         h3("Query:"),
@@ -52,6 +45,12 @@ shinyUI(navbarPage("NBLAST on-the-fly",
       mainPanel(
         HTML(paste0("<style>", paste0("tr:nth-child(", 2:11, ") { color: ", rainbow(10, alpha=NULL), "; }", collapse="\n"), "</style>")),
         h2("3D view"),
+        ###################
+        # Loading spinner #
+        ###################
+        includeCSS("loader.css"),
+        HTML("<div class='loader' style='position: absolute; left: 400px; top: 300px; z-index: -10000;'>Loading...</div>"),
+        
         webGLOutput("brain3d_all", width="800px", height="800px"),
         h2("NBLAST results"),
         HTML("<a href='http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/www/how/'>What do these scores mean?</a>"),
@@ -75,12 +74,6 @@ shinyUI(navbarPage("NBLAST on-the-fly",
   tabPanel("Pairwise comparison",
     sidebarLayout(  
       sidebarPanel(
-        ###################
-        # Loading spinner #
-        ###################
-        includeCSS("loader.css"),
-        HTML("<div class='loader' style='position: absolute; left: 940px; top: 300px; z-index: -10000;'>Loading...</div>"),
-        
         h3("Instructions"),
         HTML("Select two FlyCircuit neurons to compare with NBLAST. The <span style='color: red;'>query neuron will be plotted in red</span> in the 3D viewer to the right, while the <span style='color: blue;'>target neuron will be drawn in blue</span>."),
         h3("Query:"),
@@ -109,6 +102,12 @@ shinyUI(navbarPage("NBLAST on-the-fly",
       
       mainPanel(
         h2("3D view"),
+        ###################
+        # Loading spinner #
+        ###################
+        includeCSS("loader.css"),
+        HTML("<div class='loader' style='position: absolute; left: 400px; top: 300px; z-index: -10000;'>Loading...</div>"),
+        
         webGLOutput("brain3d_one", width="800px", height="800px"),
         h2("NBLAST results"),
         textOutput("nblast_results_one"),
@@ -124,12 +123,6 @@ shinyUI(navbarPage("NBLAST on-the-fly",
   tabPanel("Upload a tracing",
     sidebarLayout(  
       sidebarPanel(
-        ###################
-        # Loading spinner #
-        ###################
-        includeCSS("loader.css"),
-        HTML("<div class='loader' style='position: absolute; left: 940px; top: 300px; z-index: -10000;'>Loading...</div>"),
-        
         h3("Instructions"),
         HTML("Upload a tracing to compare against all FlyCircuit cluster examplars (or all neurons, if checkbox below is ticked), using NBLAST. The query neuron will be <b><span style='color: black;'>plotted in black</span></b> in the 3D viewer to the right, alongside the top 10 hits (rainbow coloured from <span style='color: red;'>red = best</span> to <span style='color: #FF0099;'>pink = worst</span>)."),
         h3("Query"),
@@ -144,6 +137,12 @@ shinyUI(navbarPage("NBLAST on-the-fly",
       mainPanel(
         HTML(paste0("<style>", paste0("tr:nth-child(", 2:11, ") { color: ", rainbow(10, alpha=NULL), "; }", collapse="\n"), "</style>")),
         h2("3D view"),
+        ###################
+        # Loading spinner #
+        ###################
+        includeCSS("loader.css"),
+        HTML("<div class='loader' style='position: absolute; left: 400px; top: 300px; z-index: -10000;'>Loading...</div>"),
+        
         webGLOutput("brain3d_tracing", width="800px", height="800px"),
         h2("NBLAST results"),
         HTML("<a href='http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/www/how/'>What do these scores mean?</a>"),
