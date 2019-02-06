85ddc67e9b982ff0312e5b258251a11a24221614
Gregory Jefferis <jefferis@gmail.com>
2019-02-06 19:16:47 +0000

    fix remotesync of dps object
    was getting > remotesync(dps)
     Show Traceback
    
     Rerun with Debug
     Error in dirname(remote) : a character vector argument expected
    
    * apparently dpscanon_f9dc90ce5b2ffb74af37db1e3a2cb35b.rds is missing an attribute with

--- download_data.R
+++ download_data.R
@@ -5,7 +5,12 @@
 library(flycircuit)
 
 message("Downloading (updated) data objects to: ", getOption("flycircuit.datadir"))
-dps=load_si_data("dpscanon_f9dc90ce5b2ffb74af37db1e3a2cb35b.rds")
+
+# this is effectively just a dummy dps object that should trigger download of
+# all files
+# a slightly different one is requested in server.R
+dps<-read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/dpscanon.rds",
+											 + 	localdir=getOption('flycircuit.datadir'))
 remotesync(dps, download.missing = TRUE)
 fc_download_data('http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/allbyallblastcanon_f9dc90ce5b2ffb74af37db1e3a2cb35b.desc', type = 'bigmat')
 apres16k.p0=load_si_data("apres16k.p0.rds")
